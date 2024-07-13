d4dff33a779a791f8f32a46617ad70231431742a
stephenmuhindi <bteneson42@gmail.com>
2024-07-13 08:12:00 +0300

    added button, arrow,signout element, admin signout navigationbar layout config

---
+++ swaggy/components/navigationbar.tsx
@@ -0,0 +1,36 @@
+import { ArrowRightSquareIcon } from "lucide-react"
+import Maxwidthwrapper from "./maxwidthwrapper"
+import { buttonVariants } from "./ui/button"
+
+const navbar = () => {
+    const user = undefined
+    const isAdmin = false
+    return(
+    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-blue-500 bg-slate/75 backdrop-blur-lg transition-all">
+        <Maxwidthwrapper>
+            <div className="h-16 items-center justify-between border-b border-zinc-500 ">
+                <link href="/" className="flex z-40 font-bold">
+                    swaggy<span className="text-purple-600">case</span>
+                    </link>
+            <div className="h-full items-center space-x-4 ">
+                {user ? (
+                    <>
+                    <link href="/api/auth/logout" className={buttonVariants({ size:"sm", variant:"ghost"})}>
+                        sign out
+                    </link>
+                    {isAdmin ? <link href="/api/auth/logout" className={buttonVariants({ size:"sm", variant:"ghost"})}>
+                        Dashboard
+                    </link> : null}
+                    <link href="/configure/upload" className={buttonVariants({ size:"sm", className: "hidden sm:flex items-center gap-1"})}>
+                        customize case
+                        <ArrowRightSquareIcon className="ml-1.5 h-5 w-5"/>
+                    </link>
+                    </>
+                ): (<></>)}
+            </div>
+            </div>
+        </Maxwidthwrapper>
+    </nav>
+    )
+}
+export default navbar
--- swaggy/components/phone.tsx
+++ swaggy/components/phone.tsx
@@ -6,7 +6,7 @@ interface phoneprops extends HTMLAttributes<HTMLDivElement>{
 }
 const phone = ({ imgsrc, className, dark = false, ...props }: phoneprops) => {
     <div className={cn("relative-events-pointer-none z-50 overflow-hidden", className)}{...props}>
-        <img src={dark ? "/phone-template-dark-edges.jpg": "/phone-template-white-edge.jpg" alt="phone images"} className="pointers-events-none z-50 select-none" />
+        <img src={dark ? "/phone-template-dark-edges.jpg": "/phone-template-white-edge.jpg" } className="pointers-events-none z-50 select-none" />
         <div className="absolute -z-10 inset-0">
             <img className="object-cover" src={imgsrc} alt="photo overlay" />
         </div>
